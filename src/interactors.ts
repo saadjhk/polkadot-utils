e254c9fc7fa792bfd70f857bb13935aaca35eb93
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-01-24 13:47:18 +0500

    Linter fix.

--- src/interactors.ts
+++ src/interactors.ts
@@ -1,22 +1,22 @@
-import { ApiPromise } from "@polkadot/api";
-import { AnyTuple, IEvent } from "@polkadot/types/types";
-import { SubmittableExtrinsic, AddressOrPair } from "@polkadot/api/types";
+import { ApiPromise } from '@polkadot/api';
+import { AnyTuple, IEvent } from '@polkadot/types/types';
+import { SubmittableExtrinsic, AddressOrPair } from '@polkadot/api/types';
 
 export async function sendUnsignedAndWaitForSuccess<T extends AnyTuple>(
-    api: ApiPromise,
-    filter: (event: IEvent<AnyTuple>) => event is IEvent<T>,
-    call: SubmittableExtrinsic<"promise">
+  api: ApiPromise,
+  filter: (event: IEvent<AnyTuple>) => event is IEvent<T>,
+  call: SubmittableExtrinsic<'promise'>
 ): Promise<IEvent<T>> {
-    return await sendUnsignedAndWaitFor(api, filter, call);
+  return await sendUnsignedAndWaitFor(api, filter, call);
 }
 
 export async function sendAndWaitForSuccess<T extends AnyTuple>(
-    api: ApiPromise,
-    sender: AddressOrPair,
-    filter: (event: IEvent<AnyTuple>) => event is IEvent<T>,
-    call: SubmittableExtrinsic<"promise">
+  api: ApiPromise,
+  sender: AddressOrPair,
+  filter: (event: IEvent<AnyTuple>) => event is IEvent<T>,
+  call: SubmittableExtrinsic<'promise'>
 ): Promise<IEvent<T>> {
-    return await sendAndWaitFor(api, sender, filter, call);
+  return await sendAndWaitFor(api, sender, filter, call);
 }
 
 /**
@@ -27,35 +27,37 @@ export async function sendAndWaitForSuccess<T extends AnyTuple>(
  * @returns event that fits the filter
  */
 export function sendUnsignedAndWaitFor<T extends AnyTuple>(
-    api: ApiPromise,
-    filter: (event: IEvent<AnyTuple>) => event is IEvent<T>,
-    call: SubmittableExtrinsic<"promise">
+  api: ApiPromise,
+  filter: (event: IEvent<AnyTuple>) => event is IEvent<T>,
+  call: SubmittableExtrinsic<'promise'>
 ): Promise<IEvent<T>> {
-    return new Promise<IEvent<T>>((resolve, reject) => {
-        call.send((res) => {
-            const { dispatchError, status } = res;
-            if (dispatchError) {
-                if (dispatchError.isModule) {
-                    const decoded = api.registry.findMetaError(dispatchError.asModule);
-                    const { docs, name, section } = decoded;
-                    reject(Error(`${section}.${name}: ${docs.join(" ")}`));
-                } else {
-                    reject(Error(dispatchError.toString()));
-                }
-            }
-            if (status.isInBlock || status.isFinalized) {
-                // @ts-ignore
-                const event = res.events.find((e) => filter(e.event)).event;
-                if (filter(event)) {
-                    resolve(event);
-                } else {
-                    reject(Error("Event record not found"));
-                }
-            }
-        }).catch((e) => {
-            reject(Error(e.message));
-        });
-    });
+  return new Promise<IEvent<T>>((resolve, reject) => {
+    call
+      .send((res) => {
+        const { dispatchError, status } = res;
+        if (dispatchError) {
+          if (dispatchError.isModule) {
+            const decoded = api.registry.findMetaError(dispatchError.asModule);
+            const { docs, name, section } = decoded;
+            reject(Error(`${section}.${name}: ${docs.join(' ')}`));
+          } else {
+            reject(Error(dispatchError.toString()));
+          }
+        }
+        if (status.isInBlock || status.isFinalized) {
+          // @ts-ignore
+          const event = res.events.find((e) => filter(e.event)).event;
+          if (filter(event)) {
+            resolve(event);
+          } else {
+            reject(Error('Event record not found'));
+          }
+        }
+      })
+      .catch((e) => {
+        reject(Error(e.message));
+      });
+  });
 }
 
 /**
@@ -67,35 +69,37 @@ export function sendUnsignedAndWaitFor<T extends AnyTuple>(
  * @returns event that fits the filter
  */
 export function sendAndWaitFor<T extends AnyTuple>(
-    api: ApiPromise,
-    sender: AddressOrPair,
-    filter: (event: IEvent<AnyTuple>) => event is IEvent<T>,
-    call: SubmittableExtrinsic<"promise">
+  api: ApiPromise,
+  sender: AddressOrPair,
+  filter: (event: IEvent<AnyTuple>) => event is IEvent<T>,
+  call: SubmittableExtrinsic<'promise'>
 ): Promise<IEvent<T>> {
-    return new Promise<IEvent<T>>((resolve, reject) => {
-        call.signAndSend(sender, { nonce: -1 }, (res) => {
-            const { dispatchError, status } = res;
-            if (dispatchError) {
-                if (dispatchError.isModule) {
-                    // for module errors, we have the section indexed, lookup
-                    const decoded = api.registry.findMetaError(dispatchError.asModule);
-                    const { docs, name, section } = decoded;
-                    reject(Error(`${section}.${name}: ${docs.join(" ")}`));
-                } else {
-                    reject(Error(dispatchError.toString()));
-                }
-            }
-            if (status.isInBlock || status.isFinalized) {
-                // @ts-ignore
-                const event = res.events.find((e) => filter(e.event)).event;
-                if (filter(event)) {
-                    resolve(event);
-                } else {
-                    reject(Error("Event record not found"));
-                }
-            }
-        }).catch((e) => {
-            reject(Error(e.message));
-        });
-    });
+  return new Promise<IEvent<T>>((resolve, reject) => {
+    call
+      .signAndSend(sender, { nonce: -1 }, (res) => {
+        const { dispatchError, status } = res;
+        if (dispatchError) {
+          if (dispatchError.isModule) {
+            // for module errors, we have the section indexed, lookup
+            const decoded = api.registry.findMetaError(dispatchError.asModule);
+            const { docs, name, section } = decoded;
+            reject(Error(`${section}.${name}: ${docs.join(' ')}`));
+          } else {
+            reject(Error(dispatchError.toString()));
+          }
+        }
+        if (status.isInBlock || status.isFinalized) {
+          // @ts-ignore
+          const event = res.events.find((e) => filter(e.event)).event;
+          if (filter(event)) {
+            resolve(event);
+          } else {
+            reject(Error('Event record not found'));
+          }
+        }
+      })
+      .catch((e) => {
+        reject(Error(e.message));
+      });
+  });
 }
