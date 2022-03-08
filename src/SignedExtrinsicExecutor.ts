227e9115d4f10a70c3d2b1b6b3505918f45c3a82
Saad Ahmed Siddiqui <48211799+saadjhk@users.noreply.github.com>
2022-03-08 14:25:44 +0500

    f

--- src/SignedExtrinsicExecutor.ts
+++ src/SignedExtrinsicExecutor.ts
@@ -3,36 +3,38 @@ import { ISubmittableResult } from '@polkadot/types/types';
 import { SubmittableExtrinsic, AddressOrPair } from '@polkadot/api/types';
 
 export class SignedExtrinsicExecutor {
-    api: ApiPromise;
-    call: SubmittableExtrinsic<'promise'>;
-    sender: AddressOrPair;
+  api: ApiPromise;
+  call: SubmittableExtrinsic<'promise'>;
+  sender: AddressOrPair;
 
-    onFinality: (blockHash: string) => void;
-    unsubscribe: any;
-    
-    constructor(
-        polkadotApi: ApiPromise,
-        call: SubmittableExtrinsic<'promise'>,
-        sender: AddressOrPair,
-        onFinality: (blockHash: string) => void
-    ) {
-        this.api = polkadotApi;
-        this.call = call;
-        this.sender = sender;
+  onFinality: (blockHash: string) => void;
+  unsubscribe: any;
 
-        this.onFinality = onFinality;
-    }
+  constructor(
+    polkadotApi: ApiPromise,
+    call: SubmittableExtrinsic<'promise'>,
+    sender: AddressOrPair,
+    onFinality: (blockHash: string) => void
+  ) {
+    this.api = polkadotApi;
+    this.call = call;
+    this.sender = sender;
 
+    this.onFinality = onFinality;
+  }
 
-    public async executeTransaction(): Promise<void> {
-        this.unsubscribe = await this.call.signAndSend(this.sender, (res: ISubmittableResult) => {
-            const { status } = res;
+  public async executeTransaction(): Promise<void> {
+    this.unsubscribe = await this.call.signAndSend(
+      this.sender,
+      (res: ISubmittableResult) => {
+        const { status } = res;
 
-            if (status.asFinalized) {
-                this.onFinality(status.asFinalized.toString());
+        if (status.asFinalized) {
+          this.onFinality(status.asFinalized.toString());
 
-                this.unsubscribe();
-            }
-        })
-    }
+          this.unsubscribe();
+        }
+      }
+    );
+  }
 }
